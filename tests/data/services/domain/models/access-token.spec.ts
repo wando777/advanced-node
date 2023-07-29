6cd450ca7bbf0bf903bed4caafea85c511bea49c
wando777 <wanderson.eq2011@gmail.com>
2023-07-29 17:43:45 -0300

    feat: ensure access token expires in within 30 minutes

--- tests/data/services/domain/models/access-token.spec.ts
+++ tests/data/services/domain/models/access-token.spec.ts
@@ -3,11 +3,15 @@ import { AccessToken } from '@/domain/models'
 describe('AccessToken', () => {
   let sut: AccessToken
   const input = 'any_value'
+  const expirationTimeInMs = 30 * 60 * 1000
   beforeEach(() => {
     sut = new AccessToken(input)
   })
 
   it('Should create a new token with a value', async () => {
     expect(sut).toEqual({ value: input })
+  })
+  it('Should verify the token expiration within 30 minutes', async () => {
+    expect(AccessToken.expirationInMs).toBe(expirationTimeInMs)
   })
 })
