343a7c4375b93f3c3f1020574b77350ba3e2e224
wando777 <wanderson.eq2011@gmail.com>
2023-08-07 22:31:28 -0300

    feat: ensure FacebookLoginController returns 401 when authentication fails

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,3 +1,4 @@
+import { AuthenticationError } from '@/domain/errors'
 import { type FacebookAuthentication } from '@/domain/features'
 import { mock, type MockProxy } from 'jest-mock-extended'
 
@@ -27,17 +28,37 @@ describe('FacebookLoginController', () => {
   })
   it('should call FacebookAuthentication with correct params', async () => {
     await sut.handle({ token: 'any_token' })
+
     expect(facebookAuth.perform).toHaveBeenCalledWith({ token: 'any_token' })
+  })
+  it('should returns 401 if FacebookAuthentication fails', async () => {
+    facebookAuth.perform.mockResolvedValueOnce(new AuthenticationError())
+
+    const httpResponse = await sut.handle({ token: 'any_token' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 401,
+      data: new AuthenticationError()
+    })
   })
 })
 
 class FacebookLoginController implements Controller {
   constructor(private readonly facebookAuth: FacebookAuthentication) { }
   async handle(httpRequest: any): Promise<HttpResponse> {
-    await this.facebookAuth.perform({ token: httpRequest.token })
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!httpRequest?.token || Object.keys(httpRequest.token).length === 0) {
+      return {
+        statusCode: 400,
+        data: new Error('A valid token must be provided')
+      }
+    }
+
+    const res = await this.facebookAuth.perform({ token: httpRequest.token })
+
     return {
-      statusCode: 400,
-      data: new Error('A valid token must be provided')
+      statusCode: 401,
+      data: res
     }
   }
 }
