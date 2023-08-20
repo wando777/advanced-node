99b25b116da10be5ed4d75e2584d27ab67734c5e
wanderson aragão <wanderson.leite@vtex.com>
2023-08-20 19:55:35 -0300

    ensure handle does not throw when receiving empty req

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -1,17 +1,32 @@
 import { type Request, type Response } from 'express'
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { type Controller } from '@/application/controllers'
-import { mock } from 'jest-mock-extended'
+import { type MockProxy, mock } from 'jest-mock-extended'
 
-describe('', () => {
+describe('ExpressRouter', () => {
+  let req: Request
+  let res: Response
+  let controller: MockProxy<Controller>
+  let sut: ExpressRouter
+  beforeEach(() => {
+    req = getMockReq({ body: { any: 'any' } })
+    res = getMockRes().res
+    controller = mock()
+    sut = new ExpressRouter(controller)
+  })
   it('should call handle with correct request', async () => {
-    const req = getMockReq({ body: { any: 'any' } })
-    const { res } = getMockRes()
-    const controller = mock<Controller>()
-    const sut = new ExpressRouter(controller)
     await sut.adapter(req, res)
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
+    expect(controller.handle).toHaveBeenCalledTimes(1)
+  })
+  it('should call handle with empty request', async () => {
+    const req = getMockReq()
+
+    await sut.adapter(req, res)
+
+    expect(controller.handle).toHaveBeenCalledWith({})
+    expect(controller.handle).toHaveBeenCalledTimes(1)
   })
 })
 
