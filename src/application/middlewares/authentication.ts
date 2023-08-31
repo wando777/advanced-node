594d267821e33563aca45b8141878eb4fe7743fe
wanderson aragão <wanderson.leite@vtex.com>
2023-08-31 09:34:23 -0300

    refactor: move all the files from authentication middleware to its own file

---
+++ src/application/middlewares/authentication.ts
@@ -0,0 +1,32 @@
+import { type Authorize } from '@/domain/use-cases'
+import { type HttpResponse, forbidden, ok } from '../helpers'
+import { Required } from '../validation'
+
+type HttpRequest = {
+  authorization: string
+}
+
+type Model = Error | { userId: string }
+
+export class AuthenticationMiddleware {
+  constructor(private readonly authorize: Authorize) {
+  }
+
+  async handle({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
+    const isError = this.validate({ authorization })
+    if (isError) {
+      return forbidden()
+    }
+    try {
+      const userId = await this.authorize({ token: authorization })
+      return ok({ userId })
+    } catch {
+      return forbidden()
+    }
+  }
+
+  private validate({ authorization }: HttpRequest): boolean {
+    const error = new Required(authorization, 'authorization').validate()
+    return !(error === undefined)
+  }
+}
--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,7 +1,5 @@
 import { ForbiddenError } from '@/application/errors'
-import { type HttpResponse, forbidden, ok } from './../../../src/application/helpers/https'
-import { type Authorize } from '@/domain/use-cases'
-import { Required } from '@/application/validation'
+import { AuthenticationMiddleware } from '@/application/middlewares'
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware
   let authorize: jest.Mock
@@ -50,32 +48,3 @@ describe('AuthenticationMiddleware', () => {
     expect(httpResponse).toEqual({ statusCode: 200, data: { userId: 'any_userId' } })
   })
 })
-
-type HttpRequest = {
-  authorization: string
-}
-
-type Model = Error | { userId: string }
-
-export class AuthenticationMiddleware {
-  constructor(private readonly authorize: Authorize) {
-  }
-
-  async handle({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
-    const isError = this.validate({ authorization })
-    if (isError) {
-      return forbidden()
-    }
-    try {
-      const userId = await this.authorize({ token: authorization })
-      return ok({ userId })
-    } catch {
-      return forbidden()
-    }
-  }
-
-  private validate({ authorization }: HttpRequest): boolean {
-    const error = new Required(authorization, 'authorization').validate()
-    return !(error === undefined)
-  }
-}
