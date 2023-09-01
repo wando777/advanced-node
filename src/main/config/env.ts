9949835c2c36c8e05083a296a5b95321db66aeb7
wanderson aragão <wanderson.leite@vtex.com>
2023-09-01 09:05:07 -0300

    create a .env file to hide credentials

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,8 @@
 export default {
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '1001036524549960',
-    clientSecret: process.env.FB_SECRET_ID ?? '389cddbfdc9f6bd63e84a27e5eefdd26'
+    clientId: process.env.FB_CLIENT_ID ?? '',
+    clientSecret: process.env.FB_SECRET_ID ?? '',
+    accessToken: process.env.FB_ACCESS_TOKEN ?? ''
   },
   port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET ?? 'tokenjwt'
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -13,7 +13,7 @@ describe('Facebook Api integration tests', () => {
     })
   })
   it('should return a Facebook User if token is valid', async () => {
-    const fbUser = await sut.loadUser({ token: 'EAAOObZCpz90gBO6WyjV5IExwwnGKrnSVZC2x1xytgR5J6Pro9OQvl1679vDgOYhqJtUZCNftkO195kFINZCo5TPZCI1kotEqP2FYjrOoHk8ZCAnXZCx8dDrZCvUEmZBWLZAlUDEvdm54t5QXCVvFYyW6PwuyDWjOXzoZBefsWd1gm8PS3prM4osgV0KHgEGU4lWrJ7TXHNHS3Qbcvu7cAQE1asuWuHHJL9dHYVj64iyIt6RmCuBiaRgRWevtZBQvw4dRJAZDZD' })
+    const fbUser = await sut.loadUser({ token: env.facebookApi.accessToken })
 
     expect(fbUser).toEqual({
       id: '7184434554919380',
