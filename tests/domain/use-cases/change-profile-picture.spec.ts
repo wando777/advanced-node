dcacae778c59997bfb4f30c03df463299d896cf1
wanderson aragão <wanderson.leite@vtex.com>
2023-09-07 09:36:43 -0300

    ensure ChangeProfilePicture calls SaveUserPicture with correct initials

--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -38,7 +38,7 @@ describe('ChangeProfilePicture', () => {
   it('should call SaveUserPicture (repository) with correct input', async () => {
     await sut({ id: 'any_userId', file })
 
-    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: 'any_url' })
+    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: 'any_url', initials: undefined })
     expect(userProfileRepo.savePicture).toHaveBeenCalledTimes(1)
   })
   it('hould call SaveUserPicture (repository) with correct input when file is undefined', async () => {
