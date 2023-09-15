c937a6b7b7d18be6437a021fee50dc09fd965a59
wanderson aragão <wanderson.leite@vtex.com>
2023-09-15 09:06:05 -0300

    ensure SavePictureController returns 400 when file is not provided

---
+++ tests/application/controllers/save-picture.spec.ts
@@ -0,0 +1,38 @@
+import { Controller } from '@/application/controllers'
+import { RequiredFieldError } from '@/application/errors'
+import { type HttpResponse, badRequest } from '@/application/helpers'
+
+describe('SavePictureController', () => {
+  let sut: SavePictureController
+  beforeEach(() => {
+    jest.clearAllMocks()
+    sut = new SavePictureController()
+  })
+  it('should extends Controller', async () => {
+    expect(sut).toBeInstanceOf(Controller)
+  })
+  it('should return 400 if file is not provided', async () => {
+    const httpResponse = await sut.handle({ file: undefined })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+  it('should return 400 if file is not provided', async () => {
+    const httpResponse = await sut.handle({ file: null })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+})
+
+type HttpRequest = { file: any }
+
+export class SavePictureController extends Controller {
+  async perform({ file }: HttpRequest): Promise<HttpResponse> {
+    return badRequest(new RequiredFieldError('file'))
+  }
+}
