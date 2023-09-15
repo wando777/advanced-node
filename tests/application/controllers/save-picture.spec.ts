7c57977c079687fbf15e8c7a12992030b62f1e92
wanderson aragão <wanderson.leite@vtex.com>
2023-09-15 09:29:18 -0300

    ensure SavePictureController returns 400 if file is empty

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -27,9 +27,17 @@ describe('SavePictureController', () => {
       data: new RequiredFieldError('file')
     })
   })
+  it('should return 400 if file is empty', async () => {
+    const httpResponse = await sut.handle({ file: { buffer: Buffer.from('') } })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
 })
 
-type HttpRequest = { file: any }
+type HttpRequest = { file: { buffer: Buffer } }
 
 export class SavePictureController extends Controller {
   async perform({ file }: HttpRequest): Promise<HttpResponse> {
