39f537bcefb43b1a73f9aa19d1f6d22c99ee9ea9
wanderson aragão <wanderson.leite@vtex.com>
2023-09-21 08:00:19 -0300

    ensure PUT /users/picture return 200 on success

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,9 +1,9 @@
 import { type Router } from 'express'
 import { auth } from '../middlewares'
 import { makeSavePictureController } from '../factories/application/controllers'
-import { adaptExpressRoute as adapt } from '../adapters'
+import { adaptExpressRoute as adapt, adaptMulter as multer } from '../adapters'
 
 export default (router: Router): void => {
   router.delete('/users/picture', auth, adapt(makeSavePictureController()))
-  router.put('/users/picture', auth)
+  router.put('/users/picture', auth, multer, adapt(makeSavePictureController()))
 }
--- tests/main/routes/user.spec.ts
+++ tests/main/routes/user.spec.ts
@@ -44,22 +44,30 @@ describe('User Routes', () => {
   })
 
   describe('PUT /users/picture', () => {
+    const uploadSpy = jest.fn()
+    jest.mock('@/infra/gateways/aws-s3-file-storage', () => ({
+      AwsS3FileStorage: jest.fn().mockReturnValue({
+        upload: uploadSpy
+      })
+    }))
     it('should return 403 if no authorization header is present', async () => {
       const { status } = await request(app)
         .put('/api/users/picture')
 
       expect(status).toBe(403)
     })
-    // it('should return 200', async () => {
-    //   const { userId } = await pgUserRepo.save({ email: 'any_email', name: 'Wando Aragao' })
-    //   const authorization = sign({ key: userId }, env.jwtSecret)
+    it('should return 200', async () => {
+      uploadSpy.mockReturnValueOnce('any_url')
+      const { userId } = await pgUserRepo.save({ email: 'any_email', name: 'Wando Aragao' })
+      const authorization = sign({ key: userId }, env.jwtSecret)
 
-    //   const { status, body } = await request(app)
-    //     .delete('/api/users/picture')
-    //     .set({ authorization })
+      const { status, body } = await request(app)
+        .put('/api/users/picture')
+        .set({ authorization })
+        .attach('picture', Buffer.from('any_buffer'), { filename: 'any_name', contentType: 'image/png' })
 
-    //   expect(status).toBe(200)
-    //   expect(body).toEqual({ pictureUrl: undefined, initials: 'WA' })
-    // })
+      expect(status).toBe(200)
+      expect(body).toEqual({ pictureUrl: 'any_url', initials: undefined })
+    })
   })
 })
