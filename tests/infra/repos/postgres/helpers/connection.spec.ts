f71f68c71718229934e3a310fc531d949328800f
wanderson aragão <wanderson.leite@vtex.com>
2023-09-23 11:33:20 -0300

    refactor: isolate const

--- tests/infra/repos/postgres/helpers/connection.spec.ts
+++ tests/infra/repos/postgres/helpers/connection.spec.ts
@@ -1,11 +1,50 @@
 /* eslint-disable @typescript-eslint/no-extraneous-class */
+import { createConnection, getConnectionManager } from 'typeorm'
+import { mocked } from 'jest-mock'
+
+jest.mock('typeorm', () => ({
+  Entity: jest.fn(),
+  PrimaryGeneratedColumn: jest.fn(),
+  Column: jest.fn(),
+  createConnection: jest.fn(),
+  getConnectionManager: jest.fn()
+}))
 
 describe('PgConnection', () => {
+  let sut: PgConnection
+  let getConnectionManagerSpy
+  let createQueryRunnerSpy: jest.Mock
+  let createConnectionSpy: jest.Mock
+
+  beforeAll(() => {
+    getConnectionManagerSpy = jest.fn().mockReturnValue({
+      has: jest.fn().mockReturnValueOnce(false)
+    })
+    mocked(getConnectionManager).mockImplementation(getConnectionManagerSpy)
+    createQueryRunnerSpy = jest.fn()
+    createConnectionSpy = jest.fn().mockResolvedValue({
+      createQueryRunner: createQueryRunnerSpy
+    })
+    mocked(createConnection).mockImplementation(createConnectionSpy)
+  })
+  beforeEach(() => {
+    jest.clearAllMocks()
+    sut = PgConnection.getInstance()
+  })
   it('should have only one instance (Singleton)', () => {
-    const sut = PgConnection.getInstance()
     const sut2 = PgConnection.getInstance()
 
     expect(sut).toBe(sut2)
+  })
+  it('should create a new connection', async () => {
+    const sut = PgConnection.getInstance()
+
+    await sut.connect()
+
+    expect(createConnectionSpy).toHaveBeenCalledWith()
+    expect(createConnectionSpy).toHaveBeenCalledTimes(1)
+    expect(createQueryRunnerSpy).toHaveBeenCalledWith()
+    expect(createQueryRunnerSpy).toHaveBeenCalledTimes(1)
   })
 })
 
@@ -19,5 +58,10 @@ class PgConnection {
       PgConnection.instance = new PgConnection()
     }
     return PgConnection.instance
+  }
+
+  async connect(): Promise<void> {
+    const connection = await createConnection()
+    connection.createQueryRunner()
   }
 }
