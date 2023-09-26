84d490cb47d97a35c62dce35bc72065a729232e8
wanderson aragão <wanderson.leite@vtex.com>
2023-09-26 09:17:28 -0300

    ensure DbTransactionController calls perform from decoratee with correct inputs

--- tests/application/decorators/db-tranaction-controller.spec.ts
+++ tests/application/decorators/db-tranaction-controller.spec.ts
@@ -1,27 +1,41 @@
+import { type Controller } from '@/application/controllers'
 import { type MockProxy, mock } from 'jest-mock-extended'
 
 describe('DbTransactionController', () => {
   let sut: DbTransactionController
   let db: MockProxy<DbTransaction>
+  let decoratee: MockProxy<Controller>
 
   beforeAll(() => {
     db = mock()
+    decoratee = mock()
   })
   beforeEach(() => {
     jest.clearAllMocks()
-    sut = new DbTransactionController(db)
+    sut = new DbTransactionController(decoratee, db)
   })
   it('shoudld open transaction', async () => {
     await sut.perform({ any: 'any' })
 
     expect(db.openTransaction).toHaveBeenCalled()
   })
+  it('shoudld calls our decoratee', async () => {
+    await sut.perform({ any: 'any' })
+
+    expect(decoratee.perform).toHaveBeenCalledWith({ any: 'any' })
+    expect(decoratee.perform).toHaveBeenCalledTimes(1)
+  })
 })
 
 class DbTransactionController {
-  constructor(private readonly db: DbTransaction) { }
+  constructor(
+    private readonly decoratee: Controller,
+    private readonly db: DbTransaction
+  ) { }
+
   async perform(httpRequest: any): Promise<void> {
     await this.db.openTransaction()
+    await this.decoratee.perform(httpRequest)
   }
 }
 
